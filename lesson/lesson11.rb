fc4f4821ad411821bce375999c244195a62e7f27
yashi8596 <deletio.422shinta@gmail.com>
2022-08-13 10:02:26 +0000

    [Finish] 2-12

--- lesson/lesson11.rb
+++ lesson/lesson11.rb
@@ -1,17 +1,17 @@
 class Car
   def run(distance)
-    
+
     puts "車で#{distance}キロ走ります。"
-    
+
   end
 end
 
 class Truck < Car
   def run(distance)
-    
+
     super
     puts "大きな荷物を載せて走ります。"
-    
+
   end
 end
 
