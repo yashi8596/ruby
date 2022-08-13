388cb3a549063c21ba194b44e2db48e97ec4ddfe
yashi8596 <deletio.422shinta@gmail.com>
2022-08-13 09:31:48 +0000

    [Finish] 2-10

---
+++ 10/inheritance.rb
@@ -0,0 +1,21 @@
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+=begin
+class Bus
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+=end
+
+class Bus < Car
+end
+
+bus = Bus.new
+bus.run(5)
+
+puts Bus.superclass
